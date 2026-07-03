b2ed0134529faf9fc054353a4a84a420eec763dc
vextrixes <113197359+vextrixes@users.noreply.github.com>
2026-07-03 16:53:30 +0200

    added from_u8 to effects enum

--- src/rgb_controller/effects.rs
+++ src/rgb_controller/effects.rs
@@ -54,4 +54,30 @@ impl Effects {
             StaticPerKey => 20,
         }
     }
+    
+    pub fn from_u8(n: u8) -> Option<Self> {
+        match n {
+            1 => Some(Static),
+            2 => Some(SingleOn),
+            3 => Some(SingleOff),
+            4 => Some(Glittering),
+            5 => Some(Rain),
+            6 => Some(Colorful),
+            7 => Some(Breath),
+            8 => Some(Spectrum),
+            9 => Some(CentrifugalWave),
+            10 => Some(VerticalWave),
+            11 => Some(HorizontalWave),
+            12 => Some(Rotating),
+            13 => Some(Explosion),
+            14 => Some(Launch),
+            15 => Some(Ripples),
+            16 => Some(Snake),
+            17 => Some(Pulse),
+            18 => Some(Tilt),
+            19 => Some(Shuttle),
+            20 => Some(StaticPerKey),
+            _ => None
+        }
+    }
 }
